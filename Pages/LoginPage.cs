81661a59cb3122f215287ae6475dce374ab7af77
Hemali <infohemalipatel29@gmail.com>
2020-08-17 16:02:00 +1000

    Implement Nunit

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TurnUp_POM.Pages
+{
+    class LoginPage
+    {
+
+        //Steps to login to Turnup portal
+        public void LoginSteps(IWebDriver driver)
+        {
+            
+            Thread.Sleep(1000);
+            //open the turn up portal 
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+            Thread.Sleep(1000);
+
+            //maximize the browser
+            driver.Manage().Window.Maximize();
+
+            //find the username textbox and enter username
+
+            IWebElement username = driver.FindElement(By.Id("UserName"));
+            username.SendKeys("hari");
+            //find password textbox and enter password
+
+            IWebElement password = driver.FindElement(By.Id("Password"));
+            password.SendKeys("123123");
+
+            //find the logi  button
+            IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginbutton.Click();
+
+            IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+            //option1 --Validate if the text on the hyperlink is Hello hari!
+
+            if (hellohari.Text == "Hello hari!")
+            {
+
+                //Console.WriteLine("logged in succefully, test passed");
+                Assert.Pass("logged in succefully, test passed");
+            }
+
+            else
+            {
+                //Console.WriteLine("logged in not succefully,test failed");
+                Assert.Fail("logged in not succefully,test failed");
+            }
+
+            //option2 --Validate if the text on the hyperlink is Hello hari!
+            Assert.That(hellohari.Text, Is.EqualTo("Hello hari!"));
+
+        }
+    }
+}
